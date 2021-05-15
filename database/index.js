e629b0527ed0cf229ff23f0d1bb6a581db28b1f1
Jared Absher <jared.absher@gmail.com>
2021-05-15 17:01:17 -0400

    new db location

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const AutoIncrementFactory = require('mongoose-sequence');
-const mongoURI = 'mongodb:/18.116.41.196/sdc';
+const mongoURI = 'mongodb://18.116.41.196:27017/sdc';
 
 const db = mongoose.connect(mongoURI, { useNewURLParser: true, useUnifiedTopology: true })
 
